cdd3f1159b13e5550aa58d12349d001f22cf1f1a
Ricardo Alberti <ricardo.alberti.26@gmail.com>
2024-01-22 22:01:53 -0300

    Initial commit

---
+++ Chess-Computer-Overlord/Program.cs
@@ -0,0 +1,58 @@
+﻿
+public class Game
+{
+    string[] board = 
+
+    { "TNBQKBCT",
+      "PPPPPPPP",
+      "########",
+      "###00###",
+      "###00###",
+      "########",
+      "PPPPPPPP",
+      "TNBQKBCT",
+    };
+
+    const char knight = 'N';
+    const char Tower = 'T';
+    const char Bishop = 'B';
+    const char King = 'K';
+    const char Queen = 'Q';
+    const char Pawn = 'P';
+    const char CenterTile = '0';
+
+    int[,] knightMoves =
+    {
+        //(x, y)
+        { 3, 1 },
+        { 3, -1 },
+
+        { -3, 1 },
+        { -3, -1 },
+
+        { 1, 3 },
+        { -1, 3 },
+
+        { 1, -3 },
+        {-1, -3 },
+    };
+
+    int[,] pawnMoves =
+    {
+        //(x, y)
+        { 1, 0 },
+        { 1, 1 },
+        { -1, 1 },
+    };
+
+    int[,] bishopMoves =
+    {
+        { 1, 1 },
+        { -1, 1 },
+        { -1, -1 },
+        { 1, -1 },
+    };
+
+    //To be continue...
+
+}
